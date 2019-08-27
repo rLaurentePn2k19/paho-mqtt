9680c133540f95360bac729ac7c306a9f95dadce
rLaurentePn2k19 <rangie.laurente@student.passerellesnumeriques.org>
2019-08-27 16:02:55 +0800

    Successfull connect and publish

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,6 +1,8 @@
 // Create a client instance
 console.log("helloworld");
-var client;
+// var client;
+var btn_connect = document.getElementById("connect-btn");
+var btn_publish = document.getElementById("publish-btn");
 
 client = new Paho.Client("broker.hivemq.com", 8000, "clientId");
 
@@ -8,18 +10,17 @@ client = new Paho.Client("broker.hivemq.com", 8000, "clientId");
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
-// connect the client
-client.connect({onSuccess:onConnect});
-
+// // connect the client
+// client.connect({onSuccess:onConnect});
 
 // called when the client connects
 function onConnect() {
   // Once a connection has been made, make a subscription and send a message.
   console.log("onConnect");
   client.subscribe("World");
-  message = new Paho.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
+//   message = new Paho.Message("Hello");
+//   message.destinationName = "World";
+//   client.send(message);
 }
 
 // called when the client loses its connection
@@ -33,3 +34,19 @@ function onConnectionLost(responseObject) {
 function onMessageArrived(message) {
   console.log("onMessageArrived:"+message.payloadString);
 }
+
+//add Event to button if click
+btn_connect.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log("connect");
+    // connect the client
+    client.connect({onSuccess:onConnect});
+})
+
+btn_publish.addEventListener('click',function(e){
+    e.preventDefault();
+    console.log("publish");
+    message = new Paho.Message("Hello");
+    message.destinationName = "World";
+    client.send(message);
+})
